105041a1a9bd416ebbe1fb72bcac2d2df6f5af7f
Jack Filipanits <78802283+Lynden-Sylvester@users.noreply.github.com>
2024-08-08 22:47:15 -0400

    Updated console_database_microservice

--- console_database_microservice.py
+++ console_database_microservice.py
@@ -1,5 +1,8 @@
 import zmq
 import sqlite3
+import json
+from datetime import datetime, timedelta
+import os
 
 def console_database_processor():
 
@@ -7,12 +10,28 @@ def console_database_processor():
     socket = context.socket(zmq.REP)
     socket.connect("tcp://localhost:5557")
 
+    directory = './'
+    filename = 'db_log.txt'
+    file_path = os.path.join(directory, filename)
     while True:
 
         message = socket.recv_json()
         print(f"Received Request: {message}")
 
-        reply = f"console database processed: {message}"
+        processed = datetime.now()
+        database_logger = {
+            "ConsoleInput": message,
+            "Process Date": processed
+        }
+        if not os.path.isfile(file_path):
+            with open(file_path, 'w') as file:
+                file.write('Test')
+            print(f"File {filename} created")
+        else:
+            with open(filename, 'a') as file:
+                file.write(f"\n{database_logger}\n")
+
+        reply = f"console database processed: {database_logger}"
         socket.send_json(reply)
 
 console_database_processor()
